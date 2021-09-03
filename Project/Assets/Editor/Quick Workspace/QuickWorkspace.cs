1c98d2f1814e12dd968a7a5981cc7bcd2ec5c6dd
Huntrt <huntrtdev@gmail.com>
2021-09-03 18:08:13 +0700

    Setup Project

---
+++ Project/Assets/Editor/Quick Workspace/QuickWorkspace.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+
+public class QuickWorkspace : EditorWindow
+{
+	//When click on Tool > Open Code in menu item
+	[MenuItem("Tools/Open Code")] static void ShowWindow()
+	{
+		//Open the workspace file if user click this button
+		AssetDatabase.OpenAsset(AssetDatabase.LoadAssetAtPath
+		//Search for the workspace location and open it
+		("Assets/Editor/Quick Workspace/Project Workspace.code-workspace", typeof(DefaultAsset)));
+	}
+}
+
+[InitializeOnLoad] class LaunchWorkspace
+{
+	const string launched = ""; static LaunchWorkspace()
+	{
+		//If haven't launch the workspace
+		if(!SessionState.GetBool(launched, false))
+		{
+			//Has launch the workspace
+			SessionState.SetBool(launched, true);
+			//Open the workspace file if user click this button
+			AssetDatabase.OpenAsset(AssetDatabase.LoadAssetAtPath
+			//Search for the workspace location and open it
+			("Assets/Editor/Quick Workspace/Project Workspace.code-workspace", typeof(DefaultAsset)));
+		}
+	}
+}
