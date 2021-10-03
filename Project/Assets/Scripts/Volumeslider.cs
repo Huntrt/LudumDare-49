5cc99102541a0a526822b0acaf6dfb8279ad7858
Huntrt <huntrt.business@outlook.com>
2021-10-04 01:09:06 +0700

    Sound slider

---
+++ Project/Assets/Scripts/Volumeslider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Volumeslider : MonoBehaviour
+{
+	[SerializeField] Slider slider;
+
+    void Start()
+    {
+		//Update the current volume
+        slider.value = SoundManager.i.volume;
+    }
+
+    void Update()
+    {
+		//Update the manager volume
+        SoundManager.i.volume = slider.value;
+    }
+}
