2ca99455201f13e07bf85dd5f64f91ea07c0b628
Huntrt <huntrt.business@outlook.com>
2021-10-03 20:03:37 +0700

    Main menu and pausing complete

---
+++ Project/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+	float recentTime;
+
+	public void ToMenu() {SceneManager.LoadScene("Menu", LoadSceneMode.Single);}
+	public void ToGame() {SceneManager.LoadScene("Game", LoadSceneMode.Single);}
+	
+	//Save the recent time and pause the game
+	public void Pause() {recentTime = Time.timeScale; Time.timeScale = 0;}
+	//Continue back to the recent time
+	public void Continue() {Time.timeScale = recentTime;}
+}
