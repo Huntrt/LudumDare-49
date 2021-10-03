a23a36054f81edf05f53b65710ec9111d9f858dd
Huntrt <huntrtdev@gmail.com>
2021-10-04 01:09:06 +0700

    Sound slider

--- Project/Assets/Scripts/SoundManager.cs
+++ Project/Assets/Scripts/SoundManager.cs
@@ -4,9 +4,19 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager i;
 	public AudioSource source;
+	public float volume;
 
 	void Awake()
 	{
-		i = this;
+		//Create singelton and don't destroy on load when neede
+		if(SoundManager.i == null) {i = this;DontDestroyOnLoad(this);}
+		//If the game mananger are not in dontdestroyonload than destroy it
+		if(gameObject.scene.name != "DontDestroyOnLoad") {Destroy(gameObject);}
+	}
+
+	void Update()
+	{
+		//Update volum
+		source.volume = volume;
 	}
 }
